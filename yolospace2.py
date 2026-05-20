1cd3f7e8616de17db272f21a7b172db5bb75ad8a
Mizuno-b <mizuno@bishamon.co.jp>
2026-05-20 15:26:04 +0900

    コンテナをブラウザ用にアップデート

--- yolospace2.py
+++ yolospace2.py
@@ -6,11 +6,11 @@ sudo docker run --runtime nvidia -it --rm \
     --privileged \
     --device /dev/video0 \
     --device /dev/video1 \
+    --device /dev/video2 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.Xauthority:/root/.Xauthority \
     -v /home/mizuno/stereo_camera_calibration.npz:/workspace/stereo_camera_calibration.npz \
     -e DISPLAY=$DISPLAY \
     -e XAUTHORITY=/root/.Xauthority \
     -e PYTORCH_NVML_DISABLE=1 \
-    my_yolo_env:v2_engine
-non-network local connections being added to access control list
+    my_yolo_env:v3_flask
