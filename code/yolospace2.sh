84a3197ee55d21b432cc8241dc74eb4c2aae9f56
Mizuno-b <mizuno@bishamon.co.jp>
2026-05-27 08:45:09 +0900

    コンテナ内でgpio使用可能にに変更

--- code/yolospace2.sh
+++ code/yolospace2.sh
@@ -7,10 +7,12 @@ sudo docker run --runtime nvidia -it --rm \
     --device /dev/video0 \
     --device /dev/video1 \
     --device /dev/video2 \
+    -v /proc/device-tree/compatible:/proc/device-tree/compatible:ro \
+    -v /dev:/dev \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $HOME/.Xauthority:/root/.Xauthority \
     -v /home/mizuno/stereo_camera_calibration.npz:/workspace/stereo_camera_calibration.npz \
     -e DISPLAY=$DISPLAY \
     -e XAUTHORITY=/root/.Xauthority \
     -e PYTORCH_NVML_DISABLE=1 \
-    my_yolo_env:v3_flask
+    my_yolo_env:v4_gpio
